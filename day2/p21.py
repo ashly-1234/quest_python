45de1dd1dc0102f12f3a74e3f4972f7b53c62548
ashly-1234 <ashly.joshy5@gmail.com>
2024-08-01 18:51:18 +0530

    update

--- day2/p21.py
+++ day2/p21.py
@@ -1,1 +1,15 @@
 # 6. Print the Pascals triangle of n lines
+
+
+from math import factorial
+
+lines = int(input("Enter the number of lines for Pascal Triangle: "))
+for i in range(lines):
+    for j in range(lines-i+1):
+        print(end=" ")
+
+    for j in range(i+1):
+        # nCr = n!/((n-r)!*r!) => oCo 1Co 1C1 2C0 2C1 2C2 ...... 
+        print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
+
+    print()
